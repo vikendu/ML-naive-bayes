503d52a4ad34b9ddbb9dd6a02fb7248226bfff59
vikendu <vikendu@gmail.com>
2023-11-05 00:44:15 +0530

    proposed structure

---
+++ naive_bayes.py
@@ -0,0 +1,24 @@
+#Whether or not we can play golf given weather conditions
+
+#dataset:
+
+# features = np.array([
+#     ["Sunny", "Hot", "High", "False"],
+#     ["Overcast", "Mild", "Normal", "True"],
+#     ["Rainy", "Cool", "Normal", "False"],
+#     ["Sunny", "Mild", "Normal", "True"],
+#     ["Rainy", "Hot", "High", "False"]
+# ])
+# labels = np.array(["Yes", "Yes", "No", "Yes", "No"])
+
+# TODO: Read the dataset 
+
+# TODO: Calculate Mean
+
+# TODO: Calculate Std Deviation
+
+# TODO: Calculate probability of each predictor variable(so the sunny/hot etc??)
+
+# TODO: Calculate Likelihood for each class(is a class YES/NO??)
+
+# TODO: Return the greatest likelihood
